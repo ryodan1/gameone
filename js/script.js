703dd21513b54def17924fc4dcf3eaa64e6730ff
Rafael Lopes Caetano <rafaellcx52@gmail.com>
2023-03-13 20:20:34 -0300

    fps

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@ const canvasHeight = 576
 canvas.width = canvasWidth
 canvas.height = canvasHeight
 
-can.fillRect(0,0, canvasWidth, canvasHeight)
+
+
+function animation(){
+    window.requestAnimationFrame(animation);
+    can.fillRect(0,0, canvasWidth, canvasHeight)
+    let delta = (performance.now() - prevTime) / 1000
+    let fps = 1 / delta
+    prevTime = performance.now()
+}
